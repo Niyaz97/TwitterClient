da586b37b179b249611e79b07bf7087416ff40ff
Niyaz97 <niyaz160297@gmail.com>
2016-10-26 18:40:46 +0300

    Update client.hpp

--- include/twitter/client.hpp
+++ include/twitter/client.hpp
@@ -1,4 +1,17 @@
 #ifndef TWITTERCLIENT_CLIENT_HPP
 #define TWITTERCLIENT_CLIENT_HPP
 
+#include <string>
+#include <map>
+
+namespace Twitter {
+	class Client {
+	public:
+		using dict_t = std::map<std::string, std::string>;
+   		Client(dict_t settings);
+   		auto check_connection() -> bool;
+	};
+}
+
+
 #endif //TWITTERCLIENT_CLIENT_HPP
