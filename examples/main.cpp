6711810f395897b72dee1c9b0f53bb0d3452746f
Niyaz97 <niyaz160297@gmail.com>
2016-11-09 22:59:05 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <twitter/client.hpp>
 
 int main() {
+    Twitter::Client check;
+    check.check_connection();
 
+    Twitter::Client followers;
+    followers.get_followers();
 }
