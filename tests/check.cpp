aacd31ea118b8b28f45f6ecb44f5ecf5c1f590e1
Niyaz97 <niyaz160297@gmail.com>
2016-11-11 00:00:55 +0300

    Update check.cpp

--- tests/check.cpp
+++ tests/check.cpp
@@ -1,1 +1,31 @@
+#include <twitter/client.hpp>
+#include "catch.hpp"
 
+SCENARIO("tokens are valid")
+{
+    GIVEN("valid consumer_key and consumer_secret")
+    {
+        Twitter::Client client;
+        WHEN("checking connection")
+        {
+            THEN("success")
+            {
+                REQUIRE(client.check_connection(cons_key, cons_secret));
+            }
+        }
+    }
+}
+SCENARIO("tokens are not valid")
+{
+    GIVEN("not valid consumer_key and consumer_secret")
+    {
+        Twitter::Client client;
+        WHEN("checking connection")
+        {
+            THEN("failure")
+            {
+                REQUIRE_FALSE(client.check_connection("Wm5teEJzN1liSTdvRDJibjVETWlyQligjlfeeqfVSRA", "Wm5hfowefjteEJzN1liSTdvRDJibjVETWlyQlVSRA"));
+            }
+        }
+    }
+}
