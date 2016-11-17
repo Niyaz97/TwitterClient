6910c96c469e89adf2c511ac874ac4664ac6809e
Niyaz97 <niyaz160297@gmail.com>
2016-11-17 13:00:12 +0300

    Update check_API.cpp

--- tests/check_API.cpp
+++ tests/check_API.cpp
@@ -7,11 +7,13 @@ SCENARIO("get followers correctly")
     {
         Twitter::Client client;
         Twitter::Client:: json followers= R"([{"followers_count":66,"id":2202089073,"name":"timagol1k","screen_name":"74Toddler"}])"_json;
+        std::cout<<followers<<std::endl;
         WHEN("get followers")
         {
             Twitter::Client::json followers_from_get = client.get_followers();
             bool are_same = (followers == followers_from_get);
-
+            std::cout<<followers_from_get<<std::endl;
+            
             THEN("json objects are the same")
             {
                 REQUIRE(are_same);
