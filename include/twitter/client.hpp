9c92117b41cf6f7865e6e48a2804a0afd96289bb
Niyaz97 <niyaz160297@gmail.com>
2016-11-17 11:24:20 +0300

    Update client.hpp

--- include/twitter/client.hpp
+++ include/twitter/client.hpp
@@ -4,26 +4,27 @@
 #include <string>
 #include <map>
 #include <curl/curl.h>
-
+#include <json.hpp>
 
 namespace Twitter {
 	class Client {
 	public:
+        using json=nlohmann::json;
         Client();
         ~Client();
-        using dict_t = std::map<std::string, std::string>;
-        Client(dict_t settings) : _settings(settings) {}
+
+        auto get_key(const std::string ) -> std::string;
+        auto get_secret(const std::string ) -> std::string;
 
         auto encode64(const std::string )->std::string;
    		auto check_connection(const std::string , const std::string ) -> bool;
-        auto get_followers() -> void;
+        auto get_followers() -> json;
 
         auto check_connection_signature() -> bool;
 
         static auto write_to_string(void* data, size_t size, size_t nmemb, void* stream) -> size_t;
 
     private:
-        dict_t _settings;
         std::string bearer_token;
         CURL* Handle;
     };
@@ -32,6 +33,8 @@ namespace Twitter {
 
 const std::string cons_key="ZnmxBs7YbI7oD2bn5DMirBURD";
 const std::string cons_secret="lmeLD9xncbPf63SLyKPII99PyplOvHu9tanFaT8vONyzMahnOc";
+
+
 
 
 #endif //TWITTERCLIENT_CLIENT_HPP
