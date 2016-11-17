b8f3f78eedaefb7a1ac50dcd10d97949c17ca65a
Niyaz97 <niyaz160297@gmail.com>
2016-11-17 13:41:06 +0300

    Update check_API.cpp

--- tests/check_API.cpp
+++ tests/check_API.cpp
@@ -6,13 +6,18 @@ SCENARIO("get followers correctly")
     GIVEN("authorized client and json object with his followers")
     {
         Twitter::Client client;
-        Twitter::Client:: json followers= R"([{"followers_count":66,"id":2202089073,"name":"timagol1k","screen_name":"74Toddler"}])"_json;
-        std::cout<<followers<<std::endl;
+  //      Twitter::Client:: json followers= R"([{"followers_count":66,"id":2202089073,"name":"timagol1k","screen_name":"74Toddler"}])"_json;
         WHEN("get followers")
         {
             Twitter::Client::json followers_from_get = client.get_followers();
-            bool are_same = (followers == followers_from_get);
-            std::cout<<followers_from_get<<std::endl;
+           bool are_same;
+                for(json::iterator it = jsn_users.begin(); it!=jsn_users.end(); ++it) {
+
+                    json jsn_follow_c=it.value()["followers_count"];
+                    json jsn_id = it.value()["id"];
+                    json jsn_name = it.value()["name"];
+                    json jsn_screen_name = it.value()["screen_name"];
+                    if(jsn_follow_c==66 && jsn_id==2202089073 && jsn_name=="timagol1k" && jsn_screen_name=="74Toddler") are_same=true;
             
             THEN("json objects are the same")
             {
