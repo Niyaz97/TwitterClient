ae6b018bbf04b50d4bc371d3e6e592d8f29675fc
Niyaz97 <niyaz160297@gmail.com>
2016-11-25 20:21:41 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,34 +3,33 @@
 
 int main() {
     Twitter::Client check;
-    check.check_connection(cons_key,cons_secret);
+    if(check.check_connection(cons_key,cons_secret))
+        std::cout<<"Connected successfully"<<std::endl;
 
     Twitter::Client followers;
     followers.get_followers();
     std::vector<Twitter::Follower> follows = followers.get_followers();
 
+    Twitter::Client print_follows;
+    size_t number;
     bool _v;
     std::string s;
     std::cout<<"Enter a command"<<std::endl;
     std::cin>> s;
     if(s=="get_followers_v"){
         _v=true;
-        size_t number;
-        std::cout <<  "Number of threads:" << std::endl;
-        std::cin >> number;
-
-        Twitter::Client thread;
-        thread.print_followers_thread(number, follows, _v);
+        if(print_follows.print_followers_thread(number, follows, _v))
+            std::cout<<"Printed successfully"<<std::endl;
     }
 
     if(s=="get_followers"){
         _v=false;
-        size_t number;
-        std::cout <<  "Number of threads:" << std::endl;
-        std::cin >> number;
+        if(print_follows.print_followers_thread(number, follows, _v))
+            std::cout<<"Printed successfully"<<std::endl;
+    }
 
-        Twitter::Client thread;
-        thread.print_followers_thread(number, follows, _v);
+    else{
+        std::cout<<"Wrong command"<<std::endl;
     }
 
     return 0;
