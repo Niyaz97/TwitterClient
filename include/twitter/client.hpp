5d7114558d371b4925143843419cbdbb20b99453
Niyaz97 <niyaz160297@gmail.com>
2016-11-25 20:20:37 +0300

    Update client.hpp

--- include/twitter/client.hpp
+++ include/twitter/client.hpp
@@ -40,7 +40,6 @@ namespace Twitter {
 
         auto get_followers() -> std::vector<Follower>;
 
-        // auto print(const Twitter::Client::json &followers, size_t threads) -> void;
         auto check_connection_signature() -> bool;
 
         static auto write_to_string(void* data, size_t size, size_t nmemb, void* stream) -> size_t;
