48729d66a5ffd44d6ea9a114513d9c599141c747
Niyaz97 <niyaz160297@gmail.com>
2016-11-25 21:37:46 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -13,24 +13,26 @@ int main() {
     Twitter::Client print_follows;
     size_t number;
     bool _v;
-    std::string s;
-    std::cout<<"Enter a command"<<std::endl;
-    std::cin>> s;
-    if(s=="get_followers_v"){
-        _v=true;
-        if(print_follows.print_followers_thread(number, follows, _v))
-            std::cout<<"Printed successfully"<<std::endl;
-    }
+    try {
+        std::string s;
+        std::cout << "Enter a command" << std::endl;
+        std::cin >> s;
+        if (s == "get_followers_v") {
+            _v = true;
+            if (print_follows.print_followers_thread(number, follows, _v))
+                std::cout << "Printed successfully" << std::endl;
+        }
 
-    if(s=="get_followers"){
-        _v=false;
-        if(print_follows.print_followers_thread(number, follows, _v))
-            std::cout<<"Printed successfully"<<std::endl;
-    }
+        if (s == "get_followers") {
+            _v = false;
+            if (print_follows.print_followers_thread(number, follows, _v))
+                std::cout << "Printed successfully" << std::endl;
+        }
 
-    else{
-        std::cout<<"Wrong command"<<std::endl;
+        else throw std::invalid_argument("Wrong command");
+        
+    } catch(const std::invalid_argument& e) {
+        std::cerr<<e.what()<<std::endl;
     }
-
     return 0;
 }
