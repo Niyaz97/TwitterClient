1d3cd9beefc59452479468d6b00a855d2aee4910
Niyaz97 <niyaz160297@gmail.com>
2016-11-25 15:34:58 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -7,12 +7,31 @@ int main() {
 
     Twitter::Client followers;
     followers.get_followers();
+    std::vector<Twitter::Follower> follows = followers.get_followers();
 
-    size_t number;
-    std::cout <<  "Number of threads" << std::endl;
-    std::cin >> number;
+    bool _v;
+    std::string s;
+    std::cout<<"Enter a command"<<std::endl;
+    std::cin>> s;
+    if(s=="get_followers_v"){
+        _v=true;
+        size_t number;
+        std::cout <<  "Number of threads:" << std::endl;
+        std::cin >> number;
 
-    Twitter::Client thread;
-    thread.print_followers_thread(number);
+        Twitter::Client thread;
+        thread.print_followers_thread(number, follows, _v);
+    }
+
+    if(s=="get_followers"){
+        _v=false;
+        size_t number;
+        std::cout <<  "Number of threads:" << std::endl;
+        std::cin >> number;
+
+        Twitter::Client thread;
+        thread.print_followers_thread(number, follows, _v);
+    }
+
     return 0;
 }
